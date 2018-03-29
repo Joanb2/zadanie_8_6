29330faef925763a26a7e993d3437a0806d98fc3
Joanna Buchała <joanna.b2@op.pl>
2018-03-29 19:21:46 +0200

    modify scripts content

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,13 @@ b = 3;
 value = (a * a) - (2 * a * b) - (b * b);
 
 console.log(value);
+
+if (value < 0) {
+	console.log("Negative results");
+}
+
+if (value = 0) {
+	console.log("Value equals 0");
+	} else {
+		console.log("Value is under 0");
+	}
